a60e26d6c6688b8d5a57a619bbe9bca95290692d
samisha yonghang <158455269+samishayonghang@users.noreply.github.com>
2024-02-05 10:17:36 +0000

    conversion

--- sam.js
+++ sam.js
@@ -1,4 +1,11 @@
 console.log("samisha")
 let accountId=20
 const accountName="mr insecure"
-console.table([accountId, accountName])
+//console.table([accountId, accountName])
+let number=30
+let stringnum= String(number)
+//console.log(stringnum)
+//console.log(typeof(stringnum))
+let func=1
+let no =Boolean(func) 
+console.log(no)
